f364b7dee163e99c21abe92edea0998c65989d6f
pablormsn <problesmansilla@uma.es>
2024-04-19 15:37:53 +0200

    practica 2 ej2

---
+++ Java/Practicas/prod_cons/Consumidor.java
@@ -0,0 +1,16 @@
+package prod_cons;
+
+public class Consumidor extends Thread {
+	private Buffer b;
+	private int it;
+	
+	public Consumidor(Buffer b, int it) {
+		this.b = b;
+		this.it = it;
+	}
+	public void run() {
+		for(int i =0; i<it; i++) {
+			int v = b.consumir();			
+		}
+	}
+}
